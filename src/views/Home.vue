57f887c65e04eb5118b777bd85efc1057abe2c76
Vinicius Vicentini <viniciusvicentini@live.com>
2020-09-28 22:17:34 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <form @submit.prevent="addNewTodo">
+    <input v-model="newTodo" ref="input" type="text" />
+
+    <button type="submit">Submit</button>
+  </form>
+
+  <ul>
+    <li
+      v-for="(todo, i) in listTodos"
+      :key="i"
+      :class="{ finished: todo.done }"
+      @click="toggleDone(todo)"
+    >
+      {{ todo.text }}
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-});
+  setup() {
+    const newTodo = ref('')
+    const listTodos = ref<Array<T>>([])
+
+    function addNewTodo() {
+      if (!newTodo.value) return
+
+      listTodos.value.push({
+        text: newTodo.value,
+      })
+
+      newTodo.value = ''
+    }
+
+    function toggleDone(todo: any) {
+      todo.done = !todo.done
+    }
+
+    return { newTodo, listTodos, addNewTodo, toggleDone }
+  },
+})
 </script>
+
+<style lang="scss" scoped>
+.finished {
+  text-decoration: line-through;
+}
+</style>
