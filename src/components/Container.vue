095c1e52d01e42f537af2730519067599252a5af
Vinicius Vicentini <viniciusvicentini@live.com>
2020-09-29 23:58:20 -0300

    container component

---
+++ src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 900px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+</style>
