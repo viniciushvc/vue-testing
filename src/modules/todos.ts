58abff4106350c09046b930c099c58bd42174e23
Vinicius Vicentini <viniciusvicentini@live.com>
2020-09-29 23:59:42 -0300

    add filteredTodos and comments

--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -1,25 +1,65 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export default function useTodos() {
+  /**
+   * Original array list
+   */
   const todos = ref<Todo[]>([])
 
-  function newTodo(todo: string) {
+  /**
+   * Filter type
+   */
+  const view = ref()
+
+  /**
+   * Show filtered TODOS
+   */
+  const filteredTodos = computed(() => {
+    switch (view.value) {
+      case 'actives':
+        return todos.value.filter(todo => !todo.done)
+      case 'done':
+        return todos.value.filter(todo => todo.done)
+      default:
+        return todos.value
+    }
+  })
+
+  /**
+   * Add new TODO to list
+   * @param text text entered by the user
+   */
+  function newTodo(text: string) {
+    if (!text) return
+
     todos.value.push({
       id: new Date().valueOf(),
-      text: todo,
+      text,
     })
   }
 
+  /**
+   * Set clicked TODO to done or not
+   * @param todo Clicked TODO
+   */
   function toggleTodo(todo: Todo) {
     todo.done = !todo.done
   }
 
+  /**
+   * Delete clicked TODO
+   * @param todo Clicked TODO
+   */
   function deleteTodo(todo: Todo) {
     todos.value = todos.value.filter(item => item.id !== todo.id)
   }
 
+  /**
+   * Return const and functions to View HOME
+   */
   return {
-    todos,
+    filteredTodos,
+    view,
     newTodo,
     toggleTodo,
     deleteTodo,
