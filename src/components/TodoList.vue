b49be39d90b42d52aeaef9f5241f948567de626c
Vinicius Vicentini <viniciusvicentini@live.com>
2020-09-30 23:42:09 -0300

    improve TodoList component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,31 +1,35 @@
 <template>
-  <div
-    v-for="todo in todos"
-    :key="todo.id"
-    class="todos"
-    @click="$emit('click', todo)"
-  >
-    <input :checked="todo.done" type="checkbox" class="todo-checkbox" />
+  <div v-for="todo in todos" :key="todo.id" class="todos">
+    <Checkbox
+      v-model="todo.done"
+      type="checkbox"
+      class="todo-checkbox"
+      @click="$emit('click', todo)"
+    />
 
     <div :class="{ 'todo-done': todo.done }" class="todo-text">
       {{ todo.text }}
     </div>
 
-    <button @click.stop="$emit('delete', todo)">Delete</button>
+    <Button @click.stop="$emit('delete', todo)">Delete</Button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import Checkbox from '@/components/Checkbox.vue'
+import Button from '@/components/Button.vue'
+
 export default defineComponent({
   props: {
-    todos: {
-      type: Array,
-      default: () => [],
-    },
+    todos: Array,
   },
   emits: ['click', 'delete'],
+  components: {
+    Checkbox,
+    Button,
+  },
 })
 </script>
 
@@ -40,7 +44,6 @@ export default defineComponent({
   margin: 10px 0;
   transition: 0.3s;
   border-radius: 5px;
-  cursor: pointer;
   background-color: #fff;
   border: 1px solid #ddd;
 
