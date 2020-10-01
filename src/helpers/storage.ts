95c06a36a0bf9a7cb23a8ec604d53c208d70630c
Vinicius Vicentini <viniciusvicentini@live.com>
2020-10-01 09:55:39 -0300

    minify getStorage code

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,11 +1,7 @@
 const STORAGE_KEY = 'TODOS'
 
 export const getStorage = () => {
-  const todos = localStorage.getItem(STORAGE_KEY)
-
-  if (todos) return JSON.parse(todos)
-
-  return []
+  return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
 }
 
 export const setStorage = (todos: Todo[]) => {
